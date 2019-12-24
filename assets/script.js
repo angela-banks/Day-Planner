2b90ff147df6c32f3031a08da72c24dde90e271e
Flowers <angela.banks25@gmail.com>
2019-12-23 17:50:29 -0800

    color changes

--- assets/script.js
+++ assets/script.js
@@ -10,25 +10,42 @@ $(".saveBtn").on("click" , function(){
 
 //Setting Current time to Day Planner
 
-let plannerHours = ["9,10,11,12,1,2,3,4,5"];
+let plannerHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 let currentTime = moment().hour();
 console.log(currentTime); //current computer is 1-hr behind
-
+// console.log($(".textBox"))
 //Change Textarea Color Based on current time
-for (i=0; i < plannerHours.length; i++) {
 
-//Append current time to textBox Row
-currentTime.append($("#textBox")).innerHTML;
-    if (plannerHours[i] < currentTime){
-        $(".textBox").css("background-color" , "lightgrey")
-    } else if (plannerHours[i] > currentTime){
-        $(".textBox").css("background-color" , "red")
-    } else{
-        $(".textBox").css("background-color" , "green");
+$(".textBox").each(function(index){
+    console.log($(this).parent().attr("id"));
+    let color = $(this).parent().attr("id");
+   // currenttime = 5
+   // current is less than the id (div) we want to make green 
+   // current is equal the id (div) we want to make red 
+   // current is greater than the id (div) we want to make grey 
+    console.log(currentTime, "COLOR",color)
+    if (currentTime < color){
+        console.log("HITTING GREEN")
+        $(this).css("background-color" , "green")
+    } else if (currentTime > color){
+        console.log("HITTING GREY")
+        $(this).css("background-color" , "lightgrey")
+    } else {
+        console.log("HITTING RED")
+        $(this).css("background-color" , "red")
     }
-};
+});
+// for (i=0; i < plannerHours.length; i++) {
 
-
-
+// //Append current time to textBox Row
+// // currentTime.append($(".textBox")).innerHTML;
+//     if (plannerHours[0] < currentTime){
+//         $(".textBox").css("background-color" , "lightgrey")
+//     } else if (plannerHours[0] > currentTime){
+//         $(".textBox").css("background-color" , "red")
+//     } else{
+//         $(".textBox").css("background-color" , "green");
+//     }
+// };
 
 
